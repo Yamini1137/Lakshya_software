1477c27ef1d5f2fc47d35c9b9d491f07d416b077
vamsireddy39 <vamsikalathur@gmail.com>
2024-06-10 18:20:30 +0530

    aboutus page additions

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -6,5 +6,38 @@ import { Component } from '@angular/core';
   styleUrl: './about.component.scss'
 })
 export class AboutComponent {
+  showme: boolean = true;
+  showmee: boolean = false;
+  showmeee: boolean = false
+
+  ngOnInit() {
+
+  }
+  toggledesign() {
+    this.showme = !this.showme
+    this.showmee = false
+    this.showmeee = false
+  }
+
+  togglemarketing() {
+    this.showmee = !this.showmee
+    this.showme = false
+    this.showmeee = false
+
+  }
+  toggledevs() {
+    this.showmeee = !this.showmeee
+    this.showme = false
+    this.showmee = false
+  }
+
+
+
+
+
+
 
 }
+
+
+
